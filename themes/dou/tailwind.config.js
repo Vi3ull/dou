6d1639c4220b5a5c99c85555b8b090a9bd556b47
Vladislav Bulanov <info@unithelp.ru>
2022-05-12 14:43:34 +0300

    init

---
+++ themes/dou/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    'layouts/**/*.{htm,js}',
+    'pages/**/*.{htm,js}',
+    'pages/**/*.{htm,js}',
+    'content/**/*.htm',
+    'partials/**/*.{htm,js}',
+    'src/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'fore-ground': '#070F27',
+      },
+    },
+  },
+  plugins: [],
+}
